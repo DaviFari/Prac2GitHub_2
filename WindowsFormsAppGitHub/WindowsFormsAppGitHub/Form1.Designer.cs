22ce9a6e29d62d6bb2181850f6b6aeb8232bcc50
davi7_64 <dffarjam2060@gmail.com>
2018-06-12 22:14:33 +0430

    add to button for change backcolor Together

--- WindowsFormsAppGitHub/WindowsFormsAppGitHub/Form1.Designer.cs
+++ WindowsFormsAppGitHub/WindowsFormsAppGitHub/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.BackColor = System.Drawing.Color.Maroon;
+            this.btn_1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_1.Location = new System.Drawing.Point(362, 113);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(152, 38);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "Click";
+            this.btn_1.UseVisualStyleBackColor = false;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(362, 228);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(152, 45);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Click";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn_1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn2;
     }
 }
 
--- WindowsFormsAppGitHub/WindowsFormsAppGitHub/Form1.cs
+++ WindowsFormsAppGitHub/WindowsFormsAppGitHub/Form1.cs
@@ -16,5 +16,21 @@ namespace WindowsFormsAppGitHub
         {
             InitializeComponent();
         }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            this.btn2.BackColor = System.Drawing.Color.Aqua;
+            this.btn2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_1.BackColor = System.Drawing.Color.Maroon;
+            this.btn_1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            this.btn_1.BackColor = System.Drawing.Color.Aqua;
+            this.btn_1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn2.BackColor = System.Drawing.Color.Maroon;
+            this.btn2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+        }
     }
 }
